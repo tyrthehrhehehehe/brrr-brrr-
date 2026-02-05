d4fee98bc7ac9084a32994535d18384a22157c19
tyrthehrhehehehe <pranjay180@gmail.com>
2026-02-05 16:43:20 +0530

    Update teeest.lua

--- teeest.lua
+++ teeest.lua
@@ -52,60 +52,7 @@ local Rock = workspace:FindFirstChild("Rocks")
 if Rock then
     Rock:Destroy()
 end
-gay = (function()
-    local lighting = game:GetService("Lighting")
-    local lightingLayers = lighting:FindFirstChild("LightingLayers")
-    if lightingLayers and game:GetService("Lighting") and game:GetService("Lighting") then
-        local darkFog = lightingLayers:FindFirstChild("DarkFog")
-        if darkFog then
-            darkFog:Destroy()
-        end
-    end
-    local Water = workspace._WorldOrigin["Foam;"]
-    if Water and workspace._WorldOrigin["Foam;"] then
-        Water:Destroy()
-    end        
-end)()
 
-LowCpu = function()
-    local decalsyeeted = true
-    local g = game
-    local w = g.Workspace
-    local l = g.Lighting
-    local t = w.Terrain
-    t.WaterWaveSize = 0
-    t.WaterWaveSpeed = 0
-    t.WaterReflectance = 0
-    t.WaterTransparency = 0
-    l.GlobalShadows = false
-    l.FogEnd = 9e9
-    l.Brightness = 0
-    settings().Rendering.QualityLevel = "Level01"
-    for i, v in pairs(g:GetDescendants()) do
-        if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
-            v.Material = "Plastic"
-            v.Reflectance = 0
-        elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
-            v.Transparency = 1
-        elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-            v.Lifetime = NumberRange.new(0)
-        elseif v:IsA("Explosion") then
-            v.BlastPressure = 1
-            v.BlastRadius = 1
-        elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-            v.Enabled = false
-        elseif v:IsA("MeshPart") then
-            v.Material = "Plastic"
-            v.Reflectance = 0
-            v.TextureID = 10385902758728957
-        end
-    end
-    for i, e in pairs(l:GetChildren()) do
-        if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
-            e.Enabled = false
-        end
-    end
-end
 CheckF = function()
     if GetBP("Dragon-Dragon") or GetBP("Gas-Gas") or GetBP("Yeti-Yeti") or GetBP("Kitsune-Kitsune") or GetBP("T-Rex-T-Rex") then
         return true
